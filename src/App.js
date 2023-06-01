d55660549bebd8df81e54bfd6f54636f5600d130
Pratham kumar <prathamkumar6699@gmail.com>
2023-06-01 22:09:26 +0530

    join page, chat and msg created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Chat from "./component/Chat/Chat";
+import Join from "./component/Join/Join";
 
 function App() {
   return (
     <div className="App">
-      <h1>working</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Join />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
